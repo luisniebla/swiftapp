c53d7027ec01386667dd59c27d725254c8306f7b
Luis Niebla <lrios@repay.com>
2023-10-11 12:27:26 -0700

    Initial Commit

---
+++ testapp/testappApp.swift
@@ -0,0 +1,17 @@
+//
+//  testappApp.swift
+//  testapp
+//
+//  Created by Luis Niebla Rios on 10/11/23.
+//
+
+import SwiftUI
+
+@main
+struct testappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
